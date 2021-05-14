40ddfae589fb26e919b46c2b8e603195a8c3f175
Johans0902 <jsperez8229@misena.edu.co>
2021-05-14 02:18:16 -0500

    Crear ClienteController

---
+++ 2184587/Controllers/ClienteController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _2184587.Models;
+
+namespace _2184587.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            using (var db = new inventarioEntities1())
+            {
+                return View(db.cliente.ToList());
+            }
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(cliente cliente)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+
+                using (var db = new inventarioEntities1())
+                {
+                    db.cliente.Add(cliente);
+                    db.SaveChanges();
+                    return RedirectToAction("index");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventarioEntities1())
+            {
+                var findUser = db.cliente.Find(id);
+                return View(findUser);
+            }
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventarioEntities1())
+                {
+                    cliente findUser = db.cliente.Where(a => a.id == id).FirstOrDefault();
+                    return View(findUser);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(cliente editUser)
+        {
+            try
+            {
+                using (var db = new inventarioEntities1())
+                {
+                    cliente user = db.cliente.Find(editUser.id);
+
+                    user.nombre = editUser.nombre;
+                    user.documento = editUser.documento;
+                    user.email = editUser.email;
+
+                    db.SaveChanges();
+                    return RedirectToAction("index");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventarioEntities1())
+                {
+                    var findUser = db.cliente.Find(id);
+                    db.cliente.Remove(findUser);
+                    db.SaveChanges();
+                    return RedirectToAction("index");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+
+        }
+
+
+    }
+}
