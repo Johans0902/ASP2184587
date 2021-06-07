4e0eecab76f5e32bc1e3d0870e7e17dd1aa4a191
Johans0902 <jsperez8229@misena.edu.co>
2021-06-07 17:37:46 -0500

    Creando un modelo custom

--- 2184587/Controllers/ProductoController.cs
+++ 2184587/Controllers/ProductoController.cs
@@ -140,6 +140,22 @@ namespace _2184587.Controllers
 
         }
 
-
+        public ActionResult Reporte()
+        {
+            var db = new inventarioEntities1();
+            {
+                var query = from tabProveedor in db.proveedor
+                            join tabProducto in db.producto on tabProveedor.id equals tabProducto.id_proveedor
+                            select new Reporte
+                            {
+                                nombreProveedor = tabProveedor.nombre,
+                                telefonoProveedor = tabProveedor.telefono,
+                                direccionProveedor = tabProveedor.direccion,
+                                nombreProducto = tabProducto.nombre,
+                                precioProducto = tabProducto.percio_unitario
+                            };
+                return View(query);
+            }
+        }
     }
 }
